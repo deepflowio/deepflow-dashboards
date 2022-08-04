3d696eb8c5d76056f15f5ccdfc3d65c60964e129
宋 建昌 <jianchang@yunshan.net>
2022-08-04 13:33:38 +0800

    init deepflow-dashboards repo

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM busybox:1.28
+COPY dashboards /dashboards
+COPY config /config
+COPY --chmod=777 copy.sh /copy.sh
+CMD ["/bin/sh", "-x" "/copy.sh"]
---
+++ copy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set +e
+cp -raf /dashboards /tmp/
+cp -raf /config/datasources/* /etc/grafana/provisioning/datasources/
+cp -raf /config/dashboards/* etc/grafana/provisioning/dashboards/
