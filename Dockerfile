fd8cf321386a2adaed10f36eb1d1770d48c7cae4
宋 建昌 <jianchang@yunshan.net>
2022-08-04 14:02:36 +0800

    update datasource.yaml

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM busybox:1.28
 COPY dashboards /dashboards
 COPY config /config
 COPY --chmod=777 copy.sh /copy.sh
-CMD ["/bin/sh", "-x" "/copy.sh"]
+CMD ["/bin/sh", "/copy.sh"]
--- copy.sh
+++ copy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set +e
+set -x
 cp -raf /dashboards /tmp/
 cp -raf /config/datasources/* /etc/grafana/provisioning/datasources/
-cp -raf /config/dashboards/* etc/grafana/provisioning/dashboards/
+cp -raf /config/dashboards/* /etc/grafana/provisioning/dashboards/
